54495819f0d06b9bb4db0ce0ba7ec0609cd1e621
bigfoot31 <probhonjonbaruah@gmail.com>
2017-09-19 12:43:31 +0530

    grid done

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -9,7 +9,42 @@ export class GameComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+
+  whiteId: number;
+  temp: number;
+
+  getRandomintInclusive(a: number, b: number) {
+    a = Math.ceil(a);
+    b = Math.floor(b);
+
+    return Math.floor(Math.random() * (b - a + 1 )) + a;
   }
 
+    ngOnInit() {
+      this.whiteId = this.getRandomintInclusive(1, 4);
+    }
+
+    getColor(id: number) {
+      if (id === this.whiteId) {
+      return 'red';
+      } else {
+      return 'black';
+      }
+    }
+
+    changeColor() {
+      this.temp = this.getRandomintInclusive(1, 4);
+
+      if (this.temp === this.whiteId) {
+        this.temp = this.getRandomintInclusive(1, 4);
+      } else {
+        this.whiteId = this.temp;
+      }
+    }
+
+    sendId (clickedId: number) {
+      if (this.whiteId === clickedId) {
+        this.changeColor();
+      }
+    }
 }
