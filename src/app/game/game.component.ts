ff84550c25c148a7dccce4fe17d5b2c5e6c9bfd6
bigfoot31 <probhonjonbaruah@gmail.com>
2017-09-19 13:03:54 +0530

    added display

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -7,23 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
-  constructor() { }
-
-
   whiteId: number;
   temp: number;
-
+  score = 0;
+  
   getRandomintInclusive(a: number, b: number) {
     a = Math.ceil(a);
     b = Math.floor(b);
-
+  
     return Math.floor(Math.random() * (b - a + 1 )) + a;
   }
-
+  
     ngOnInit() {
       this.whiteId = this.getRandomintInclusive(1, 4);
     }
-
+  
     getColor(id: number) {
       if (id === this.whiteId) {
       return 'red';
@@ -31,19 +29,20 @@ export class GameComponent implements OnInit {
       return 'black';
       }
     }
-
+  
     changeColor() {
       this.temp = this.getRandomintInclusive(1, 4);
-
-      if (this.temp === this.whiteId) {
+  
+      while (this.temp === this.whiteId)  {
         this.temp = this.getRandomintInclusive(1, 4);
-      } else {
-        this.whiteId = this.temp;
       }
+  
+      this.whiteId = this.temp;
     }
-
+  
     sendId (clickedId: number) {
       if (this.whiteId === clickedId) {
+        this.score = this.score + 1;
         this.changeColor();
       }
     }
