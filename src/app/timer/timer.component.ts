c8f04b4b22a2d137bdf045fb327d1af864d503df
shekharrajak <shekharrajak@live.com>
2017-09-19 12:39:14 +0530

    timer stop at the finishtime done

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,5 +1,7 @@
+  /* tslint:disable:no-access-missing-member */
 import { Component, OnInit } from '@angular/core';
 import { TimerService } from '../timer.service';
+import {Observable} from 'rxjs/Rx';
 
 @Component({
   selector: 'app-timer',
@@ -15,18 +17,26 @@ export class TimerComponent implements OnInit {
   // public stopwatchService: StopWatchService;
 
   public time: number;
-  public autoStart = true;
+  public autoStart = false;
 
   private timer: any;
 
   constructor(public stopwatchService: TimerService) {
+      // timer service
       this.stopwatchService = stopwatchService;
       this.time = 0;
       this.started = false;
       if (this.autoStart) {
         this.start();
       }
-  }
+
+      Observable.interval(1)
+      .subscribe(x => {
+      this.stop_on_this(999 * 2); }
+    );
+}
+
+
 
   formatTime(timeMs: number) {
       let minutes: string,
@@ -34,6 +44,11 @@ export class TimerComponent implements OnInit {
 
       minutes = Math.floor(timeMs / 60000).toString();
       seconds = ((timeMs % 60000) / 1000).toFixed(3);
+    //   console.log(+minutes);
+    //   if (+minutes === 2) {
+    //       console.log(+minutes);
+    //       this.stop();
+    //   }
       return minutes + ':' + (+seconds < 10 ? '0' : '') + seconds;
   }
 
@@ -42,6 +57,8 @@ export class TimerComponent implements OnInit {
 
       return () => {
           this.time = this.stopwatchService.time();
+        //   console.log('get update:');
+        //   console.log(this.time);
       };
   }
 
@@ -59,9 +76,20 @@ export class TimerComponent implements OnInit {
       this.update();
   }
 
+    stop_on_this(at_this_time): void {
+        if ( this.time >= at_this_time)  {
+            // console.log('hekk');
+            this.reset();
+        }
+    }
   start() {
       this.timer = setInterval(this.getUpdate(), 1);
       this.stopwatchService.start();
+        //    stop once time finished
+        // Observable.interval(1000 * 2)
+        // .subscribe(x => {
+        //     this.stop(); }
+        // );
   }
 
   stop() {
@@ -86,7 +114,7 @@ export class TimerComponent implements OnInit {
   onClick() {
       console.log(this.stopwatchService);
   }
-  
+
   ngOnInit() {
   }
 }
